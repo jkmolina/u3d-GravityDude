87ccd0c84ca6c618cf1bffc7df8157d3a0827847
Joonho D. Kim <37599608+jkmolina@users.noreply.github.com>
2020-03-12 09:05:09 -0600

    created safe repo

---
+++ u3d-FinalProject/Gravity Dude/Assets/Scripts/LoseScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player1"))
+        {
+            Debug.Log("Killed Player 1!");
+            Destroy(collision.gameObject);
+            //load scene
+        }
+        if (collision.CompareTag("Player2"))
+        {
+            Debug.Log("Killed PLayer 2!");
+            Destroy(collision.gameObject);
+            //load scene
+        }
+    }
+}
---
+++ u3d-FinalProject/Gravity Dude/Assets/Scripts/Player2.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2 : MonoBehaviour
+{
+    public int hp = 100;
+    public GameObject jugador2;
+    public int xPosition;
+    public int yPosition;
+    public int xspeed;
+    public int yspeed;
+    public Rigidbody2D rb;
+    bool isGrounded;
+    bool touchesEnemy;
+    Vector2 pos;  
+    void Start()
+    {
+        Vector2 pos = transform.position;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 tempScale = transform.localScale;
+        tempScale.y = (yspeed < 0) ? Mathf.Abs(tempScale.y) : -Mathf.Abs(tempScale.y);
+        transform.localScale = tempScale;
+        GetComponent<Rigidbody2D>().velocity = new Vector2(xspeed, yspeed);
+
+        if (isGrounded)
+        {
+            changeYourGravity();
+        }
+
+        if (touchesEnemy) 
+        {
+            changeOtherGravity();
+        }
+    }
+    public void TakeDamage(int amount)
+    {
+        hp -= amount;
+        if (hp <= 0)
+        {
+            pos.x = xPosition;
+            pos.y = yPosition;
+            transform.position = pos;
+        }
+    }
+
+    public void changeYourGravity()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            yspeed = -yspeed;
+        }
+    }
+
+
+
+    public void changeOtherGravity()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            GameObject.FindGameObjectWithTag("Player1").GetComponent<Player1>().yspeed *= -1;
+        }
+    }
+
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Platform"))
+        {
+            isGrounded = true;
+        }
+
+        if (collision.gameObject.CompareTag("Player1"))
+        {
+            touchesEnemy = true;
+            isGrounded = true;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Platform"))
+        {
+            isGrounded = false;
+        }
+        if (collision.gameObject.CompareTag("Player1"))
+        {
+            touchesEnemy = false;
+            //isGrounded = false;
+        }
+    }
+}
